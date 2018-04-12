e5c701baaee5ee652c2d3f79dcd6b8ded095a81b
hekamtbacha <a-sabur@hotmail.com>
2018-04-12 00:06:40 -0700

    working version of Json conversion

--- PycharmProjects/convertJSON/json2.py
+++ PycharmProjects/convertJSON/json2.py
@@ -8,9 +8,10 @@ filename = "/home/hakem/Desktop/netscan.json"
 data = pd.read_json(filename)
 # df =pd.DataFrame(pd.read_json(filename)['tenant_id']['hosts']['ip'] )
 li = []
-
+hosts=[]
 
 for item in data["systems"]:
+    hosts.append(item['ip'])
     for host in item["services"]:
         for CVE in host["vulnerability"]:
 
@@ -26,7 +27,7 @@ df = df[df.id.isin(b)]
 print 'test'
 
 #
-file=open("/home/hakem/Desktop/json2.p","w")
+file=open("/home/hakem/Desktop/json4.P","w")
 #
 
 scan_res= pd.DataFrame(li, columns =['ip','id','score','port','protocol','service_name'])
@@ -37,15 +38,26 @@ for index, row in final.iterrows():
     #if(row['id']==' ' or row['soft']==' ' or row['rng']==' ' or row['severity']==' ' ):
      #   print('hi')
     #else:
-        if(len(row['lose_types'].split(','))>1):
-           p.append('vulProperty(' +"'" + row['id'] +"'" + ',' + row['rng'][1:len(row['rng'])-1] + ',' + (row['lose_types'].split(',')[0][1:len(row['lose_types'])-2]).strip('\'') + ').')
-        else:
-            p.append('vulProperty(' + "'" + row['id'] + "'" + ',' + row['rng'][1:len(row['rng']) - 1] + ',' +
-                     row['lose_types'][1:len(row['lose_types']) - 1] + ').')
+        #if(len(row['lose_types'].split(','))>1):
+         #  p.append('vulProperty(' +"'" + row['id'] +"'" + ',' + row['rng'][1:len(row['rng'])-1] + ',' + (row['lose_types'].split(',')[0][1:len(row['lose_types'])-2]).strip('\'') + ').')
+        #else:
+            #if(row['lose_types'][1:len(row['lose_types']) - 1]=='availability_loss'):
+             #   p.append('vulProperty(' + "'" + row['id'] + "'" + ',' + row['rng'][1:len(row['rng']) - 1] + ',' + 'privEscalation' + ').')
+            #else:
+           #     p.append('vulProperty(' + "'" + row['id'] + "'" + ',' + row['rng'][1:len(row['rng']) - 1] + ',' + 'privEscalation' + ').')
+        #p.append('vulProperty(' + "'" + row['id'] + "'" + ',' + row['rng'][1:len(row['rng']) - 1] + ',' +
+             #         row['lose_types'][1:len(row['lose_types']) - 1] + ').')
+        p.append('cvss(' + "'" + row['id'] + "'" + ',' + row['access'] + ').')
+        p.append('vulProperty(' + "'" + row['id'] + "'" + ',' + row['rng'][1:len(row['rng']) - 1] + ',' + 'privEscalation' + ').')
 
-        p.append('vulExists(' +"'" + row['ip']+"'"  + ',' +"'" + row['id']+"'"  + ',' + row['soft'].strip('.') + ').')
-        p.append('cvss('  +"'" + row['id'] +"'" + ',' + row['access'] + ').')
-        p.append('networkServiceInfo(' +"'" +row['ip'] +"'"+ ',' + row['service_name']+ ',' +row['protocol']+ ',' + "'"+str(row['port']) +"'" +',' + 'someuser'+ ').')
+        p.append('networkServiceInfo(' + "'" + row['ip'] + "'" + ',' +"'"+ row['soft'] +"'"+ ',' + row['protocol'] + ',' + "'" + str( row['port']) + "'" + ',' + 'someuser' + ').')
+        p.append('vulExists(' +"'" + row['ip']+"'"  + ',' +"'" + row['id']+"'"  + ',' +"'"+ row['soft'].strip('.') +"'"+ ').')
+for i in range(len(hosts)):
+    victem="'"+hosts[i]+"_victem'"
+    p.append("inCompetent("+victem+").")
+    p.append("hasAccount("+victem+",'"+hosts[i]+"', user).")
+    p.append("attackerLocated(Internet).")
+    p.append("attackGoal(execCode('"+hosts[i]+"', _)).")
 p.append('hacl(_,_,_,_).')
 
 for i in range(len(p)):
